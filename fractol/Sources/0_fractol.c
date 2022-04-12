eea16e9f68d4d8d99be02ad8f4a8081c4372c91b
Matthis DoQuocBao <mat@Matthiss-Mac-mini.local>
2022-04-12 03:15:28 -0400

    test'

--- fractol/Sources/0_fractol.c
+++ fractol/Sources/0_fractol.c
@@ -31,15 +31,18 @@ void	ft_launch_minilibx(char **argv)
 	void	*mlx;
 	void	*win;
 	t_mem	save;
+	int		i;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 500, 500, argv[0]);
 	save.img = mlx_new_image(mlx, 500, 500);
 	save.addr = mlx_get_data_addr(save.img, &save.bpp, &save.l_l, &save.endian);
-	my_mlx_pixel_put(&save, 250, 250, 0x000000FF);
-	mlx_put_image_to_window(mlx, win, save.img, 0, 0);
-	//mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_key_hook(mlx->window, hook_keydown, mlx);
+	mlx_expose_hook(mlx->window, hook_expose, mlx);
+	mlx_hook(mlx->window, 4, 1L << 2, hook_mousedown, mlx);
+	mlx_hook(mlx->window, 6, 1L << 6, hook_mousemove, mlx);
 	mlx_loop(mlx);
+	return (0);
 }
 
 
--- fractol/Sources/1_Command.c
+++ fractol/Sources/1_Command.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   1_Command.c                                        :+:      :+:    :+:   */
+/*   1_hook.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdoquocb <mdoquocb@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,17 +12,4 @@
 
 #include "../Includes/fractol.h"
 
-int	deal_key(int key, void *param)
-{
-	ft_putchar('x');
-	return (0);
-}
-
-void	my_mlx_pixel_put(t_mem *save, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = save->addr + (y * save->l_l + x * (save->bpp / 8));
-	*(unsigned int*)dst = color;
-}
 
